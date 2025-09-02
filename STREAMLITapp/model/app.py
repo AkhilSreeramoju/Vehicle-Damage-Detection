ee672ec036ab098a98a1e207e72b59d7e14c6188
Akhil Sreeramoju <sreeramojuakhil111@gmail.com>
2025-09-02 23:24:11 +0530

    Refactor app.py: cache model, fix JS regex, and temp file handling

--- STREAMLITapp/model/app.py
+++ STREAMLITapp/model/app.py
@@ -6,7 +6,7 @@ st.title('Vehicle_Detection')
 uploaded = st.file_uploader('Upload image', type=['jpg','png'])
 
 if uploaded:
-    image = "tem.jgp"
+    image = "tem.jpg"
     with open(image,"wb") as f:
         f.write(uploaded.getbuffer())
         st.image(uploaded, caption='Uploaded Image',  use_container_width =True)
